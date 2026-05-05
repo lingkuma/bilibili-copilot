02b82c88fd8fa4ace12f180da414bbc3c60c516f
LingKuma <linghikki@gmail.com>
2026-05-06 02:39:51 +0800

    Add new icon images for various sizes (16px, 32px, 48px)

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
     default_locale: 'en',
     name: '__MSG_extensionName__',
     description: '__MSG_extensionDescription__',
+    icons: {
+      16: 'icons/icon16.png',
+      32: 'icons/icon32.png',
+      48: 'icons/icon48.png',
+      128: 'icons/icon128.png',
+    },
     permissions: ['storage', 'tabs'],
     host_permissions: [
       'https://www.bilibili.com/*',
@@ -17,6 +23,11 @@ export default defineConfig({
     ],
     action: {
       default_title: '__MSG_extensionName__',
+      default_icon: {
+        16: 'icons/action16.png',
+        24: 'icons/action24.png',
+        32: 'icons/action32.png',
+      },
     },
   },
 })
